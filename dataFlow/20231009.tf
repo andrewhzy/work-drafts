c5ae5b2727fa60ac7b29168b19433fcd8bb6200c
Andrew <71499897+andrewhzy@users.noreply.github.com>
2023-10-08 22:43:05 -0500

    Update 20231009.tf

--- dataFlow/20231009.tf
+++ dataFlow/20231009.tf
@@ -1,1 +1,14 @@
+provider "google" {
+  project = "<your-project-id>"
+  region  = "<your-region>"
+}
 
+resource "google_dataflow_job" "my_job" {
+  name              = "my-job"
+  template_gcs_path = "gs://path/to/your/template"
+  temp_gcs_location = "gs://path/to/your/temp/location"
+  input_file        = "gs://dataflow-samples/shakespeare/kinglear.txt"
+  output_path       = "gs://path/to/your/output"
+
+  service_account_email = "<your-service-account-email>"
+}
